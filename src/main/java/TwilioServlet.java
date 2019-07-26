e1f217b65304cfd5f8b2c10dafeb4ac262fdbe3c
Justin <justinyang1000@hotmail.com>
2019-07-26 16:23:57 -0400

    Working Version of TODO BOT Twilio

--- src/main/java/TwilioServlet.java
+++ src/main/java/TwilioServlet.java
@@ -1,55 +1,59 @@
-import java.io.IOException;
+//https://stackoverflow.com/questions/54444937/unable-to-extract-body-from-twilio-request
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
 import com.twilio.twiml.MessagingResponse;
-import com.twilio.twiml.TwiMLException;
-
 import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class TwilioServlet {
 
     public static void main(String[] args) {
-
+        //initialize base list of items
         ArrayList<String> items = new ArrayList<>();
         items.add("buy apples");
         items.add("buy peaches");
         items.add("buy oranges");
 
-
+        //web output
         get("/", (req, res) -> "Hello Web");
 
+        //method to recieve incomming messages
         post("/sms", (req, res) -> {
             res.type("application/xml");
-            System.out.println(req.body());
 
-            if(req.body().contains("Add")){
-                String messageAdd = req.body();
-                String[] messageSPLIT = messageAdd.split(" ");
-                List<String> itemList = Arrays.asList(messageSPLIT);
-                String newItem ="";
+
+            //req.body() gets the entire body of the message: ToCountry=GB&ToState=&SmsMessageSid=SMc52bea78ca1df688d...
+            //converts the body of the message filtered in a clear text
+            String bodymessage = req.queryParams("Body");
+
+//==========Add=========================================================================================================
+            if(bodymessage.contains("add")){
+                String[] messageSPLIT = bodymessage.split(" ");
+                List<String> itemList = new ArrayList<>();
+                String addItem ="";
+
+                for(int i = 0; i <messageSPLIT.length; i++)
+                {
+                        itemList.add(messageSPLIT[i]);
+                }
+                itemList.remove("add");
 
                 for(int i = 0; i<itemList.size();i++)
                 {
-                    if(itemList.get(i).equals("Add"))
+                    if(i == itemList.size()-1)
                     {
-                        itemList.remove(i);
+                        addItem += (itemList.get(i));
+                    }
+                    else
+                    {
+                        addItem += (itemList.get(i)+" ");
                     }
                 }
 
-                for(int i = 0; i<itemList.size();i++)
-                {
-                    newItem += (itemList.get(i)+" ");
-                }
-                items.add(newItem);
+                items.add(addItem);
 
                 Body body = new Body
                         .Builder("The item has been added!")
@@ -64,7 +68,9 @@ public class TwilioServlet {
                         .build();
                 return twiml.toXml();
             }
-            else if(req.body().contains("List")){
+
+//==========List========================================================================================================
+            else if(bodymessage.contains("list")){
                 String output = "";
                 for(int i = 0; i<items.size();i++)
                 {
@@ -83,9 +89,10 @@ public class TwilioServlet {
                         .build();
                 return twiml.toXml();
             }
-            else if(req.body().contains("Remove")){
-                String message = req.body();
-                String[] messageSPLIT = message.split(" ");
+
+//==========Remove======================================================================================================
+            else if(bodymessage.contains("remove")){
+                String[] messageSPLIT = bodymessage.split(" ");
                 int element = 0;
 
                 for(int i = 0; i<messageSPLIT.length;i++)
@@ -101,7 +108,7 @@ public class TwilioServlet {
                         continue;
                     }
                 }
-                items.remove(element);
+                items.remove(element-1);
                 Body body = new Body
                         .Builder("Item "+element+" was removed")
                         .build();
@@ -115,6 +122,8 @@ public class TwilioServlet {
                         .build();
                 return twiml.toXml();
             }
+
+//==========Fallback Output=============================================================================================
             else
             {
                 Body body = new Body
